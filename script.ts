4e6a4beeed3187a0e1303a238e42589358c8b3d2
topdeveloper-dev <topdeveloper7589@gmail.com>
2023-08-23 04:57:38 -0400

    Init project

---
+++ script.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Alice",
+  //     email: "alice@prisma.io",
+  //   },
+  // });
+  // const users = await prisma.user.findMany();
+  // console.log(users);
+  const user = await prisma.user.create({
+    data: {
+      name: "Bob",
+      email: "bob@prisma.io",
+      posts: {
+        create: {
+          title: "Hello World",
+        },
+      },
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
